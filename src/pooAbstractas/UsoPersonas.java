e755ca7999b0e2b716e124cde9a6584a614c2657
german <germanjongewaard@gmail.com>
2017-12-23 19:48:34 +0100

    new commit

--- src/pooAbstractas/UsoPersonas.java
+++ src/pooAbstractas/UsoPersonas.java
@@ -14,14 +14,16 @@ public class UsoPersonas {
     public static void main(String[] args) {
         // TODO code application logic here
         
+       /*  
         Empleados Juan = new Empleados("Juan", new Date(5465454), 2500);
         
         System.out.println(Juan.getNombre());
         System.out.println(Juan.getDescripcion());
         
-        Jefes Ana = new Jefes("Ana", new Date(6578976), 3000);
+        Jefes Ana = new Jefes("Ana", new Date(6578976), 3500);
+        Ana.setCargo("Directora comercial");
         System.out.println(Ana.getNombre());
-        System.out.println(Ana.getDescripcion());
+        System.out.println(Ana.getDescripcion() +  Ana.getCargo());
         
         Alumnos Maria = new Alumnos("María", "Dibujo", "Aula 5");
         
@@ -33,7 +35,24 @@ public class UsoPersonas {
         David.setCargo("Director de RRHH");
         
         System.out.println(David.getNombre());
-        System.out.println(David.getDescripcion() +  David.getCargo());
+        System.out.println(David.getDescripcion() +  David.getCargo()); 
+        */
+       
+       Empleados losEmpleados[] = new Empleados[5];
+       
+       losEmpleados[0] = new Empleados("Juan", new Date(345345), 2500);
+       losEmpleados[1] = new Empleados("María", new Date(345345), 1500);
+       losEmpleados[2] = new Empleados("Ana", new Date(345345), 3500);
+       losEmpleados[3] = new Empleados("Sandra", new Date(345345), 6000);
+       losEmpleados[4] = new Empleados("Antonio", new Date(345345), 2100);
+       
+       for(Empleados e: losEmpleados) {
+           
+           System.out.println(e.getDescripcion());
+           
+            
+       }
+       
     }
     
 }
