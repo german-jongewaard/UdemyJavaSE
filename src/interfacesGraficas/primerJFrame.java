7b10d3e83940b01c3a645fb3fde0a7b393b42780
german <germanjongewaard@gmail.com>
2018-01-22 16:09:22 +0100

    Implementación Primer JFrame (Ventana) + metodos de formato para la ventana.

--- src/interfacesGraficas/primerJFrame.java
+++ src/interfacesGraficas/primerJFrame.java
@@ -1,16 +1,33 @@
 package interfacesGraficas;
 
+ 
+import javax.swing.JFrame;
+
 /**
  *
  * @author german
+ * 
  */
 public class primerJFrame {
-
-    /**
-     * @param args the command line arguments
-     */
+     
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        JFrame ventana = new JFrame();
+        
+        ventana.setTitle("Guitar chords");
+        
+        ventana.setSize(820,300);   
+
+        ventana.setLocationRelativeTo(null);  
+        
+        ventana.setVisible(true);
+        
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     
+        
     }
+ 
     
 }
