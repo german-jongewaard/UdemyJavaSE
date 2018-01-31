1e9adf02a40b8fe9794dfbd6bfda784ea4e13657
german <germanjongewaard@gmail.com>
2018-01-31 16:30:25 +0100

    Implementación JFrame - Pruebas Básicas.

--- src/interfacesGraficas/primerJFrame.java
+++ src/interfacesGraficas/primerJFrame.java
@@ -16,8 +16,7 @@ public class primerJFrame {
         JFrame ventana = new JFrame();        
         ventana.setTitle("Guitar chords");        
         ventana.setSize(820,300);   
-        ventana.setLocationRelativeTo(null);          
-                 
+        ventana.setLocationRelativeTo(null);    
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         ventana.setVisible(true);
