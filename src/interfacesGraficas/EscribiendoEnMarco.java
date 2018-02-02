c3aa8419f9cb7597749a894e153e27b4945b838e
german <germanjongewaard@gmail.com>
2018-02-02 10:09:37 +0100

    Implementación de Marco con JFrame desde Cero.

---
+++ src/interfacesGraficas/EscribiendoEnMarco.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacesGraficas;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author german
+ */
+public class EscribiendoEnMarco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
+
+class MarcoTexto extends JFrame{
+    
+    public MarcoTexto(){
+        
+        setBounds(400,200,600,450);
+        
+        setTitle("Escritura en JFrame");
+    }
+    
+}
