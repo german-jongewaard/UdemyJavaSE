3694294e7bcc1bfae2f2740a09449725874a6d9f
german <germanjongewaard@gmail.com>
2018-02-02 10:12:03 +0100

    Implementación de Marco con JFrame desde Cero.

--- src/interfacesGraficas/EscribiendoEnMarco.java
+++ src/interfacesGraficas/EscribiendoEnMarco.java
@@ -12,6 +12,10 @@ public class EscribiendoEnMarco {
     
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        MarcoTexto miMarco = new MarcoTexto();
+        
+        miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
 }
@@ -24,6 +28,8 @@ class MarcoTexto extends JFrame{
         setBounds(400,200,600,450);
         
         setTitle("Escritura en JFrame");
+        
+        setVisible(true);
     }
     
 }
