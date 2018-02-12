e323844352553e045ce8eed25e2adcd7898006a8
german <germanjongewaard@gmail.com>
2018-02-12 13:10:59 +0100

    Implementación Dibujando en JFrame. Un rectángulo.

--- src/interfacesGraficas/DibujandoEnJFrame.java
+++ src/interfacesGraficas/DibujandoEnJFrame.java
@@ -1,5 +1,8 @@
 package interfacesGraficas;
 
+import java.awt.*;
+import javax.swing.*;
+
 /**
  *
  * @author german
@@ -10,6 +13,37 @@ public class DibujandoEnJFrame {
 
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        MarcoDibujo mimarco = new MarcoDibujo();
+        
+        mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }    
+}
+
+class MarcoDibujo extends JFrame{
     
+    public MarcoDibujo(){
+    
+        setBounds(400,200,800,450);
+        
+        setTitle("Escritura en JFrame");     
+        
+        LaminaDibujo milamina = new LaminaDibujo();
+        
+        add(milamina);
+        
+        setVisible(true);
+    }
+}
+
+class LaminaDibujo extends JPanel{
+    
+     public void paintComponent(Graphics g){
+        
+            super.paintComponent(g);
+            
+            g.draw3DRect(20, 20, 125, 75, true);
+            
+            
+     } 
 }
