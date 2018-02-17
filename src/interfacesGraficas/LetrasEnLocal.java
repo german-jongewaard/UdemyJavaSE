8b27101f9b4579dab8b0c1d3dd6d37675ccd9ad6
german <germanjongewaard@gmail.com>
2018-02-17 22:38:47 +0100

    Implementación Fuentes de texto.

--- src/interfacesGraficas/LetrasEnLocal.java
+++ src/interfacesGraficas/LetrasEnLocal.java
@@ -1,5 +1,8 @@
 package interfacesGraficas;
 
+import java.awt.Graphics;
+import java.awt.GraphicsEnvironment;
+
 /**
  *
  * @author german
@@ -8,6 +11,18 @@ package interfacesGraficas;
 
 public class LetrasEnLocal {
     
-    
-    
+    public static void main(String[] args){
+     
+        //Todas las fuenes de texto estan almacenadas en este array
+        String [] misFuentes = GraphicsEnvironment
+                .getLocalGraphicsEnvironment()
+                .getAvailableFontFamilyNames();
+        
+        for(String nombreFuente:misFuentes){
+            
+            System.out.println("Mi fuente es: " + nombreFuente);
+            
+        }    
+        
+    }
 }
