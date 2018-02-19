e1002a0ef4480b0bdce3c102f5215c4a8eaab896
german <germanjongewaard@gmail.com>
2018-02-19 13:40:53 +0100

    Implementación Manejo de Imágenes.

---
+++ src/interfacesGraficas/ManejoDeImagenes.java
@@ -0,0 +1,14 @@
+package interfacesGraficas;
+
+/**
+ *
+ * @author german
+ * 
+ */
+public class ManejoDeImagenes {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
