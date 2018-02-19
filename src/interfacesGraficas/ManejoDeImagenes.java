014032eefa831ac7282d835132635a53a265851f
german <germanjongewaard@gmail.com>
2018-02-19 14:14:02 +0100

    Implementación Manejo de Imágenes.

--- src/interfacesGraficas/ManejoDeImagenes.java
+++ src/interfacesGraficas/ManejoDeImagenes.java
@@ -1,5 +1,9 @@
 package interfacesGraficas;
 
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
  *
  * @author german
@@ -9,6 +13,33 @@ public class ManejoDeImagenes {
     
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        MarcoImagenes mimarco = new MarcoImagenes();
+        
+        mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+}
+
+class MarcoImagenes extends JFrame{
     
+    
+    public MarcoImagenes (){
+        
+     setBounds(300, 300, 800, 450);
+        
+        LaminaImagenes milamina = new LaminaImagenes();
+        
+        add(milamina);
+        
+        setVisible(true);    
+        
+    }
+}
+
+class LaminaImagenes extends JPanel{
+
+    public void paintComponent(Graphics g){
+ 
+
+    }
 }
