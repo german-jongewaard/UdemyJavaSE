0dc1f28dc98237123dfb3403f023d97427e09ef6
german <germanjongewaard@gmail.com>
2018-03-03 22:37:00 +0100

    Implementando Eventos.

--- src/eventos/EventoSencillo.java
+++ src/eventos/EventoSencillo.java
@@ -15,9 +15,7 @@ import javax.swing.JPanel;
 
 public class EventoSencillo {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
         
