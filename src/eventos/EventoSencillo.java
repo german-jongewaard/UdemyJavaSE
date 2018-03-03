3d84180aad780ebc5c188b5c0598dd4872f0d0c0
german <germanjongewaard@gmail.com>
2018-03-03 19:45:32 +0100

    Implementando Eventos.

---
+++ src/eventos/EventoSencillo.java
@@ -0,0 +1,18 @@
+package eventos;
+
+/**
+ *
+ * @author german
+ * 
+ */
+
+public class EventoSencillo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
