0861cdef8cf63e5ede6f22e68876dbc68ec77e1e
german <germanjongewaard@gmail.com>
2018-03-19 14:48:56 +0100

    Implementando Eventos de Ventana

--- src/eventos/EventoVentana.java
+++ src/eventos/EventoVentana.java
@@ -1,17 +1,27 @@
 package eventos;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author german
  * 
  */
 public class EventoVentana {
-
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        MarcoEventoVentana mimarco = new MarcoEventoVentana();
+        
+        mimarco.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
+    
+}
+
+class MarcoEventoVentana extends JFrame{
+    
+    
+    
     
 }
