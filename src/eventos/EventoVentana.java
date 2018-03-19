8e3e6233bc7d683b6da335b88a57b8621ece777f
german <germanjongewaard@gmail.com>
2018-03-19 14:47:27 +0100

    Implementando Eventos de Ventana

---
+++ src/eventos/EventoVentana.java
@@ -0,0 +1,17 @@
+package eventos;
+
+/**
+ *
+ * @author german
+ * 
+ */
+public class EventoVentana {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
