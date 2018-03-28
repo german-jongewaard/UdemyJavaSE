19c31abce48ab9a6db58a9e28f69181be82c807a
german <germanjongewaard@gmail.com>
2018-03-28 13:35:42 +0200

    Implementando Interfaces graficas - Cambio de Estados.

---
+++ src/eventos/EventoVentanaVideo88.java
@@ -0,0 +1,48 @@
+package eventos;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowStateListener;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author german
+ * 
+ */
+public class EventoVentanaVideo88 {
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        NuevoMarcoEventoVentana mimarco = new NuevoMarcoEventoVentana();
+        
+        mimarco.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        
+        mimarco.addWindowListener(new EventosDeVentana());
+    }
+    
+}
+
+class NuevoMarcoEventoVentana extends JFrame{
+    
+    
+    public NuevoMarcoEventoVentana(){
+        
+        setBounds(300,300,600,350);
+        
+        setVisible(true);
+        
+    }     
+}
+
+    class NuevoEventosDeVentana implements WindowStateListener {
+
+    @Override
+    public void windowStateChanged(WindowEvent e) {
+       
+    }
+
+     
+    
+}
