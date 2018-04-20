98b4d1237590ab2ce07252e79a2aa05b7be16d8a
german <germanjongewaard@gmail.com>
2018-04-20 12:57:14 +0200

    Implementando Evento Ratón

---
+++ src/eventos/EventoRaton.java
@@ -0,0 +1,14 @@
+package eventos;
+
+/**
+ *
+ * @author german
+ * 
+ */
+public class EventoRaton {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
