686b6d3e321c7bfe90c132726010b43aae8d43da
german <germanjongewaard@gmail.com>
2018-04-20 13:03:01 +0200

    Implementando Evento Ratón

--- src/eventos/EventoRaton.java
+++ src/eventos/EventoRaton.java
@@ -11,6 +11,11 @@ public class EventoRaton {
     
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        MarcoEventoRaton miMarco = new MarcoEventoRaton();
+        
+        miMarco.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        
     }
     
 }
@@ -20,6 +25,9 @@ class MarcoEventoRaton extends JFrame {
     
     public MarcoEventoRaton(){
         
+        setBounds(300, 300, 600, 350);
+        
+        setVisible(true);
         
     }
 }
