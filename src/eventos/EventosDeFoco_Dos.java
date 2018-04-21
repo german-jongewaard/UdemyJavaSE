b8988c650ac97060131eec75671ffd36ba8db812
german <germanjongewaard@gmail.com>
2018-04-21 03:04:37 +0200

    Implementando Evento Foco_Dos

--- src/eventos/EventosDeFoco_Dos.java
+++ src/eventos/EventosDeFoco_Dos.java
@@ -73,17 +73,22 @@ class Lamina_Marco_Foco_Dos extends JPanel{
         etiq2 = new JLabel("Contraseña");
         etiq3 = new JLabel("Email");
         
-        campoTexto1.setBounds(120, 10, 150, 20);
-        
-        etiq1.setBounds(120, 10, 150, 20);
+        campoTexto1.setBounds(120, 10, 150, 20); 
         
         campoTexto2.setBounds(120, 50, 150, 20);
         
         campoTexto3.setBounds(120, 100, 150, 20);
         
+        etiq1.setBounds(70, 10, 150, 20);
+        etiq2.setBounds(50, 50, 150, 20);
+        etiq3.setBounds(70, 90, 150, 20);
+        
         add(campoTexto1);       
         add(campoTexto2);        
         add(campoTexto3);
+        add(etiq1);
+        add(etiq2);
+        add(etiq3);
         
         EventoDeFoco foco = new EventoDeFoco();
         
