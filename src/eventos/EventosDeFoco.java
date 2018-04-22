0e050527f6949cd6f86554ad21f86d29f746968a
german <germanjongewaard@gmail.com>
2018-04-23 00:06:33 +0200

    Implementando Evento Foco

---
+++ src/eventos/EventosDeFoco.java
@@ -0,0 +1,44 @@
+package eventos;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author german
+ * 
+ */
+public class EventosDeFoco {
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        MarcoFoco miMarco = new MarcoFoco();
+        
+        miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        miMarco.setVisible(true);
+        
+    }
+    
+}
+
+class MarcoFoco extends JFrame{
+    
+    public MarcoFoco(){
+        
+        setBounds(500, 300, 400, 250);
+        
+        add(new Lamina_Marco_Foco());
+    }
+    
+}
+
+class Lamina_Marco_Foco extends JPanel{
+    
+    public void paintComponent(Graphics g){
+      
+        super.paintComponent(g);
+        
+    } 
+}
