e04040c4655fd811b12c8ecd8ad1ac7b2136e597
german <germanjongewaard@gmail.com>
2018-04-26 04:22:19 +0200

    Implementando Multiples Fuentes de Eventos

--- src/eventos/EventosMultiplesfuentes.java
+++ src/eventos/EventosMultiplesfuentes.java
@@ -1,5 +1,10 @@
 package eventos;
 
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
  *
  * @author german
@@ -10,6 +15,31 @@ public class EventosMultiplesfuentes {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        MarcoFuentes miMarcoFuente = new MarcoFuentes();
+        
+        miMarcoFuente.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        miMarcoFuente.setVisible(true);
+        
     }
     
+    
+class MarcoFuentes extends JFrame{
+    
+    public MarcoFuentes(){
+        
+        setBounds(600, 300, 500, 350);
+        
+        add(new LaminaFuentes());
+    }    
+}
+
+class LaminaFuentes extends JPanel{
+    
+    public LaminaFuentes() {
+
+    }
+
+    
+    }
 }
