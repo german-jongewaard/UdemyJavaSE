fca74ce08bf351e916db8ff08e3202d5dd321380
german <germanjongewaard@gmail.com>
2018-04-26 04:04:02 +0200

    Implementando Multiples Fuentes de Eventos

--- src/eventos/EventosMultiplesfuentes.java
+++ src/eventos/EventosMultiplesfuentes.java
@@ -6,12 +6,10 @@ package eventos;
  * 
  */
 public class EventosMultiplesfuentes {
-
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
         // TODO code application logic here
+        
     }
     
 }
