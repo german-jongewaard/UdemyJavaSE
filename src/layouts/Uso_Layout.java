f87eff99d2c051dc4f8e2d536ea39131f6e4f304
german <germanjongewaard@gmail.com>
2018-05-10 00:42:54 +0200

    Implementando Layouts

--- src/layouts/Uso_Layout.java
+++ src/layouts/Uso_Layout.java
@@ -1,5 +1,9 @@
 package layouts;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
  *
  * @author german
@@ -9,6 +13,43 @@ public class Uso_Layout {
 
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
+             MaroConLayout miMarco=new MaroConLayout();
+		
+		miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		miMarco.setVisible(true);
+
+	}
+
+}
+
+class MaroConLayout extends JFrame{
+	
+	
+	public MaroConLayout(){
+		
+		setTitle("Disposiciones");
+		
+		setBounds(600,350,600,300);
+		
+		PanelconLayout lamina=new PanelconLayout();
+		
+		add(lamina);
+	
+	}
+	
+}
+
+class PanelconLayout extends JPanel{
+	
+		public PanelconLayout(){
+			
+			add(new JButton("Amarillo"));
+			
+			add(new JButton("Rojo"));	
+			
+			add(new JButton("Azul"));	
+			
+			
+		}
 }
