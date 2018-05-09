27826e787b5369415a850610cecd2a8fb8bd5c96
german <germanjongewaard@gmail.com>
2018-05-10 00:40:41 +0200

    Implementando Layouts

---
+++ src/layouts/Uso_Layout.java
@@ -0,0 +1,14 @@
+package layouts;
+
+/**
+ *
+ * @author german
+ * 
+ */
+public class Uso_Layout {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
