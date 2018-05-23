65fd8416fc3e2804fbbf06ad8672d0a8b77d8f6c
german <germanjongewaard@gmail.com>
2018-05-23 23:50:32 +0200

    Implementando Calculadora Java

--- src/CalculadoraEjercicio/Calculadora.java
+++ src/CalculadoraEjercicio/Calculadora.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package CalculadoraEjercicio;
 
 import java.awt.BorderLayout;
@@ -112,6 +108,7 @@ class PanelCalculadora extends JPanel{
     
     private JButton pantalla;
     private JPanel numeracion;
+    private boolean comienzo;
 }
 
 
