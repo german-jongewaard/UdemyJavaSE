d6f00d3114b00576bb185250e2d4e195575b63e7
german <germanjongewaard@gmail.com>
2018-05-23 23:50:47 +0200

    Implementando Calculadora Java

--- src/CalculadoraEjercicio/Calculadora.java
+++ src/CalculadoraEjercicio/Calculadora.java
@@ -17,9 +17,7 @@ import javax.swing.JPanel;
 
 public class Calculadora {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here        
              MaroConLayout miMarco=new MaroConLayout();
