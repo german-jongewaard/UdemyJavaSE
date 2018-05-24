37466ac18abc9ac106038567b6f7a31fef1987ab
german <germanjongewaard@gmail.com>
2018-05-24 23:35:42 +0200

    Implementando Calculadora Java

--- src/CalculadoraEjercicio/Calculadora.java
+++ src/CalculadoraEjercicio/Calculadora.java
@@ -8,6 +8,8 @@ package CalculadoraEjercicio;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -114,4 +116,15 @@ class PanelCalculadora extends JPanel{
     }
     
     private JPanel numeracion;
+}
+
+
+class InsertarNumero implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+    
+    
 }
