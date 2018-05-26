659ebc0c5177ecedae776370f911e11c9128843e
german <germanjongewaard@gmail.com>
2018-05-26 13:03:44 +0200

    Implementando Componentes Swing JTextField

--- src/ComponentesSwing/ManejoCuadroTexto.java
+++ src/ComponentesSwing/ManejoCuadroTexto.java
@@ -7,10 +7,7 @@ package ComponentesSwing;
  */
 
 public class ManejoCuadroTexto {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
