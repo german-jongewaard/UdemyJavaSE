72c27fdb95ac6f3dea132bcd05339a692608779d
german <germanjongewaard@gmail.com>
2018-05-27 13:14:38 +0200

    Implementando Componentes Swing JTextField

--- src/ComponentesSwing/ManejoCuadroTexto.java
+++ src/ComponentesSwing/ManejoCuadroTexto.java
@@ -55,7 +55,7 @@ class LaminaCuadroTexto extends JPanel{
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+          
         }
         
     }
