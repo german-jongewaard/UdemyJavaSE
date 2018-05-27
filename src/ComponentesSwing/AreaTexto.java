10dd7de0c70c33f0ac87b34626a397ae07033111
german <germanjongewaard@gmail.com>
2018-05-28 00:29:56 +0200

    Implementando Componentes Swing JTextField

--- src/ComponentesSwing/AreaTexto.java
+++ src/ComponentesSwing/AreaTexto.java
@@ -7,9 +7,6 @@ package ComponentesSwing;
  */
 public class AreaTexto {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
