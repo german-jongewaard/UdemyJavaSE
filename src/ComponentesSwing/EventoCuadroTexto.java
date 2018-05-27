8dee3f2f537fc0abe24bea7dfc7fe365de6191b8
german <germanjongewaard@gmail.com>
2018-05-27 16:15:49 +0200

    Implementando Componentes Swing JTextField

--- src/ComponentesSwing/EventoCuadroTexto.java
+++ src/ComponentesSwing/EventoCuadroTexto.java
@@ -5,11 +5,9 @@ package ComponentesSwing;
  * @author german
  * 
  */
-public class EventoCuadroTexto {
 
-    /**
-     * @param args the command line arguments
-     */
+public class EventoCuadroTexto {
+   
     public static void main(String[] args) {
         // TODO code application logic here
     }
