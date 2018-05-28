a1477a58cbe767009cd63a1181ed26aebe03cfc6
german <germanjongewaard@gmail.com>
2018-05-29 00:32:05 +0200

    Implementando Componentes Swing AreaTexto

--- src/ComponentesSwing/AreaTexto.java
+++ src/ComponentesSwing/AreaTexto.java
@@ -1,5 +1,12 @@
 package ComponentesSwing;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 /**
  *
  * @author german
@@ -7,8 +14,32 @@ package ComponentesSwing;
  */
 public class AreaTexto {
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        
+        MarcoAreaTexto mimarco = new MarcoAreaTexto();
+                
+    }    
+}
+
+class MarcoAreaTexto extends JFrame{
     
+    public MarcoAreaTexto(){
+        
+        setBounds(600, 300, 600, 400);
+        
+        add(new LaminaCuadroTexto());
+        
+        setVisible(true);
+    }
+}
+
+class LaminaAreaTexto extends JPanel{
+    
+    public LaminaAreaTexto() {
+        
+        
+        
+    }
+     
 }
