e40a99a7eddb6c42497277304f52f6bfcab63dcb
german <germanjongewaard@gmail.com>
2018-05-28 16:24:52 +0200

    Implementando Componentes Swing JTextField

--- src/ComponentesSwing/EventoCuadroTexto.java
+++ src/ComponentesSwing/EventoCuadroTexto.java
@@ -1,5 +1,10 @@
 package ComponentesSwing;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 /**
  *
  * @author german
@@ -10,6 +15,32 @@ public class EventoCuadroTexto {
    
     public static void main(String[] args) {
         // TODO code application logic here
+        
+         MarcoCuadroEvento mimarco = new MarcoCuadroEvento();
+        
+        
     }
     
+}
+
+class MarcoCuadroEvento extends JFrame{
+    
+    public MarcoCuadroEvento(){
+        
+        setBounds(600, 300, 600, 400);
+        
+        add(new LaminaCuadroEvento());
+        
+        setVisible(true);
+    }
+}
+
+
+class LaminaCuadroEvento extends JPanel{
+    
+    public LaminaCuadroEvento() {
+        
+        
+        
+    }
 }
