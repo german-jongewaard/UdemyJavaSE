8b6d5ecf89c63207908940ec81f3b3070e86cfca
german <germanjongewaard@gmail.com>
2018-05-30 10:10:16 +0200

    Implementando Componentes Swing AreaTexto JTextArea

--- src/ComponentesSwing/ManejoCheckBox.java
+++ src/ComponentesSwing/ManejoCheckBox.java
@@ -1,12 +1,14 @@
 package ComponentesSwing;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import javafx.scene.control.CheckBox;
 import javafx.scene.text.Text;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -38,29 +40,36 @@ class MarcoCheckBoxEvento extends JFrame{
     }
 }
 
-
 class LaminaCheckBoxEvento extends JPanel{
     
     public LaminaCheckBoxEvento() {        
         
         setLayout(new BorderLayout());
                 
-        miTexto = new JTextField("Este texto puede cambiar...");
+        texto = new JLabel("Este texto puede cambiar...");        
+         
+        texto.setFont(new Font("Courier", Font.PLAIN, 24));
+        
+        add(texto, BorderLayout.NORTH);
 
-        miTexto.setEnabled(true); 
 
-        add(miTexto, BorderLayout.NORTH);
-                
-        JCheckBox miBoxUno = new JCheckBox("Negrita");
         
-        JCheckBox miBoxDos = new JCheckBox("Cursiva");
+        negrita = new JCheckBox("Negrita");
         
-        add(miBoxUno, BorderLayout.SOUTH);
+        cursiva = new JCheckBox("Cursiva");       
+       
+        add(cursiva, BorderLayout.SOUTH);
         
-        add(miBoxDos, BorderLayout.AFTER_LAST_LINE);
-        
+        add(negrita, BorderLayout.SOUTH);
+         
+         
         
         }
     
      private JTextField miTexto;
+     
+     private JCheckBox negrita;
+     private JCheckBox cursiva;
+     
+     private JLabel texto;
     }
