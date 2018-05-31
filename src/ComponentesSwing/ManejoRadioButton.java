527cb68e08f03d8a66740e863e188e4b571fcf1b
german <germanjongewaard@gmail.com>
2018-05-31 12:16:42 +0200

    Implementando Componentes Swing Radio Button

--- src/ComponentesSwing/ManejoRadioButton.java
+++ src/ComponentesSwing/ManejoRadioButton.java
@@ -1,5 +1,9 @@
 package ComponentesSwing;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+
 /**
  *
  * @author german
@@ -9,6 +13,29 @@ public class ManejoRadioButton {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        MarcoRadioButtonEvento mimarco = new MarcoRadioButtonEvento();
     }
     
+}
+
+class MarcoRadioButtonEvento extends JFrame{
+    
+    public MarcoRadioButtonEvento(){
+        
+        setBounds(600, 300, 600, 400); 
+        
+        add(new LaminaRadioButtonEvento());
+        
+        setVisible(true);
+    }
+}
+
+class LaminaRadioButtonEvento extends JPanel{
+    
+    public LaminaRadioButtonEvento(){
+        
+        
+    } 
+    
+    private JRadioButton boton1, boton2, boton3;
 }
