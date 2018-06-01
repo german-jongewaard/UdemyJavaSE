9016af3624ad84a7d707bd4299e482e98f2a1ccc
german <germanjongewaard@gmail.com>
2018-06-01 12:28:01 +0200

    Implementando Ejercicio 108

--- src/Ejercicios_Planteados/Video_108_Ejercicio.java
+++ src/Ejercicios_Planteados/Video_108_Ejercicio.java
@@ -1,6 +1,11 @@
 package Ejercicios_Planteados;
-
+ 
+import java.awt.Button;
+import javafx.scene.control.RadioButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  *
@@ -11,16 +16,37 @@ public class Video_108_Ejercicio {
 
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
+        
+        MarcoGeneral miMarcoGeneral = new MarcoGeneral();
+    }    
 }
 
 class MarcoGeneral extends JFrame{
     
     public MarcoGeneral(){
         
+        setBounds(600, 300, 600, 400);      
+        
+        add(new LaminaGeneralEvento());
+        
+        setVisible(true); 
+    }
+}
+
+class LaminaGeneralEvento extends JPanel{
+    
+    public LaminaGeneralEvento() {        
+ 
+        
         
     }
     
-    
+      
+     private JCheckBox casilla1;
+     private JCheckBox casilla2;     
+     private RadioButton radio1;
+     private RadioButton radio2;
+     private Button boton1;
+     private Button boton2;
+     private JLabel texto;
 }
