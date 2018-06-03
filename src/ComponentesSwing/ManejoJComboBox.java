6ed5094012d366bdf345720a44b09bb7d41ed072
german <germanjongewaard@gmail.com>
2018-06-03 17:06:00 +0200

    Implementando Manejo JComboBox

--- src/ComponentesSwing/ManejoJComboBox.java
+++ src/ComponentesSwing/ManejoJComboBox.java
@@ -3,12 +3,10 @@ package ComponentesSwing;
 /**
  *
  * @author german
+ * 
  */
 public class ManejoJComboBox {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
