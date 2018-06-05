9760d213065d995c69c06b3e37f092dfd4bc53d8
german <germanjongewaard@gmail.com>
2018-06-05 20:13:02 +0200

    Implementando Manejo JComboBox

--- src/ComponentesSwing/ManejoJComboBox.java
+++ src/ComponentesSwing/ManejoJComboBox.java
@@ -96,7 +96,7 @@ class LaminaComboBox extends JPanel{
         @Override
         public void actionPerformed(ActionEvent e) {
             
-            texto.setFont(new Font((String) miCombo.getSelectedItem(), Font.PLAIN, 18));                   
+            texto.setFont(new Font((String) miCombo.getSelectedItem(), Font.PLAIN, miSlider.getValue()));                   
         } 
     }
     
