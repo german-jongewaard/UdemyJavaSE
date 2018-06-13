1b9cca17cd3a3a9f826151c151af97277e2bea28
german <germanjongewaard@gmail.com>
2018-06-13 13:08:41 +0200

    Implementando Manejo de JSpinner

--- src/ComponentesSwing/ManejoJSpinner.java
+++ src/ComponentesSwing/ManejoJSpinner.java
@@ -25,8 +25,13 @@ class MarcoJSpinner extends JFrame{
         setBounds(600, 300, 600, 400);
         
         add(new)
-        
-        
     }
     
+}
+
+class LaminaJSpinner extends JPanel{
+    
+    public LaminaJSpinner(){
+        
+    }
 }
