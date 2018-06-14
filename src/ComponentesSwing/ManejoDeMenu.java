2884f570d657475db846eda1c54fabbf0401a763
german <germanjongewaard@gmail.com>
2018-06-15 00:01:40 +0200

    Implementando Manejo de JSpinner

--- src/ComponentesSwing/ManejoDeMenu.java
+++ src/ComponentesSwing/ManejoDeMenu.java
@@ -19,26 +19,26 @@ public class ManejoDeMenu {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-     MarcoJSpinner2 miMarco = new MarcoJSpinner2();
+     MarcoDeMenu miMarco = new MarcoDeMenu();
         miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        
     }    
 }
 
-class MarcoJSpinner2 extends JFrame{
+class MarcoDeMenu extends JFrame{
     
-    public MarcoJSpinner2(){
+    public MarcoDeMenu(){
         
         setBounds(600, 300, 600, 400);
         
-        add(new LaminaJSpinner());
+        add(new LaminaDeMenu());
         
         setVisible(true);
     }    
 }
 
-class LaminaJSpinner2 extends JPanel{
+class LaminaDeMenu extends JPanel{
     
-    public LaminaJSpinner2(){
+    public LaminaDeMenu(){
         
     }
 }
