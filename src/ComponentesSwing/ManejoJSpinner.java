6e5d2f6eb6e7274cda89bbb04b90c9471bbf390a
german <germanjongewaard@gmail.com>
2018-06-15 15:04:23 +0200

    Implementando Manejo de JSpinner

--- src/ComponentesSwing/ManejoJSpinner.java
+++ src/ComponentesSwing/ManejoJSpinner.java
@@ -43,7 +43,7 @@ class LaminaJSpinner extends JPanel{
        //JSpinner control = new JSpinner(new SpinnerListModel(meses));        
         //Dimension d = new Dimension(150, 20);
         
-        JSpinner control = new JSpinner(new SpinnerNumberModel(5, 15, -5, 1));        
+        JSpinner control = new JSpinner(new SpinnerNumberModel(5, -5, 15, 1));        
          
         control.setPreferredSize(new Dimension(150, 20));
         
