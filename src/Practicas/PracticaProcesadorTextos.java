e966be126dfe69028dcf5f4e0cc656f730e76394
german <germanjongewaard@gmail.com>
2018-07-04 13:50:28 +0200

    Implementando Práctica Procesador de Textos.

--- src/Practicas/PracticaProcesadorTextos.java
+++ src/Practicas/PracticaProcesadorTextos.java
@@ -109,7 +109,7 @@ class LaminaProcesadorTextos extends  JPanel{
              
              if(estilos == Font.BOLD) elemMenuItem.addActionListener(new StyledEditorKit.BoldAction());
              
-             else   elemMenuItem.addActionListener(new );
+             else   elemMenuItem.addActionListener(new StyledEditorKit);
          
          }else if(menu == "tamaño"){ 
              
