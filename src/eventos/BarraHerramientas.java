ffc218227c669b9e870951637a46e04fa015dc41
german <germanjongewaard@gmail.com>
2018-07-05 16:29:14 +0200

    Implementando Barra de herramientas.

--- src/eventos/BarraHerramientas.java
+++ src/eventos/BarraHerramientas.java
@@ -3,12 +3,10 @@ package eventos;
 /**
  *
  * @author german
+ * 
  */
 public class BarraHerramientas {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
