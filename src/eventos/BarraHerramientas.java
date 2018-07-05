78bd45ebb36469b2aae7ad87ef8c91f6064680a1
german <germanjongewaard@gmail.com>
2018-07-05 16:34:20 +0200

    Implementando Barra de herramientas.

--- src/eventos/BarraHerramientas.java
+++ src/eventos/BarraHerramientas.java
@@ -9,7 +9,9 @@ public class BarraHerramientas {
 
     public static void main(String[] args) {
         
+        MarcoBarra miMarco = MarcoBarra();
         
+        miMarco.setDefaultCloseOperation()
         
     }
     
