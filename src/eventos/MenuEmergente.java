eee59e59c6baeec47badee113c23a73a567c9de7
german <germanjongewaard@gmail.com>
2018-07-07 17:32:07 +0200

    Implementando Barra de herramientas.

--- src/eventos/MenuEmergente.java
+++ src/eventos/MenuEmergente.java
@@ -1,5 +1,7 @@
 package eventos;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author german
@@ -9,6 +11,25 @@ public class MenuEmergente {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        EmergenteFrame miMarcoFont = new EmergenteFrame();
+        
+        miMarcoFont.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+        
     }
+}
     
+    
+class EmergenteFrame extends JFrame{
+    
+    public EmergenteFrame(){
+        
+        setBounds(600, 300, 500, 350);
+        
+        add(new LaminaFuentes());
+        
+        setVisible(true);
+    }    
 }
