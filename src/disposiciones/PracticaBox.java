acc44b213ec7e8a57819e993ba61400d83db917a
german <germanjongewaard@gmail.com>
2018-07-09 16:44:33 +0200

    Implementando disposiciones box()

--- src/disposiciones/PracticaBox.java
+++ src/disposiciones/PracticaBox.java
@@ -1,15 +1,12 @@
-
 package disposiciones;
 
 /**
  *
  * @author german
+ * 
  */
 public class PracticaBox {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
