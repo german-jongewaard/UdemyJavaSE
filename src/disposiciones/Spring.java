6acb5ef40eb8410504568529968d03323c196bd7
german <germanjongewaard@gmail.com>
2018-07-12 01:47:01 +0200

    Implementando disposiciones Spring

--- src/disposiciones/Spring.java
+++ src/disposiciones/Spring.java
@@ -3,12 +3,10 @@ package disposiciones;
 /**
  *
  * @author german
+ * 
  */
 public class Spring {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
