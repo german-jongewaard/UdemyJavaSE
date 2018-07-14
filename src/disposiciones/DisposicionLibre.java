f547006aecf819666f54e45574e5d5cfd968f7da
german <germanjongewaard@gmail.com>
2018-07-14 03:57:09 +0200

    Implementando disposiciones Spring

--- src/disposiciones/DisposicionLibre.java
+++ src/disposiciones/DisposicionLibre.java
@@ -6,8 +6,34 @@ package disposiciones;
  */
 public class DisposicionLibre {
     
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         // TODO code application logic here
+        
+        MarcoDispLibre miMarco = new MarcoDispLibre();
+        
+        miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
 }
+
+class MarcoDispLibre extends JFrame{
+    
+    public MarcoDispLibre(){
+        
+        setBounds(300, 400, 1000, 350);
+        
+        LaminaDispLibre miLamina = new LaminaMuelle();
+        
+        add(miLamina);
+        
+        setVisible(true);
+        
+    } 
+}
+
+class LaminaMuelle extends JPanel{
+    
+    public LaminaMuelle(){
+        
+    }
+    }
