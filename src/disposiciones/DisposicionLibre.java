47642b326bc5c096c7528db69e8fa233a25dd7fa
german <germanjongewaard@gmail.com>
2018-07-14 03:56:17 +0200

    Implementando disposiciones Spring

--- src/disposiciones/DisposicionLibre.java
+++ src/disposiciones/DisposicionLibre.java
@@ -5,10 +5,7 @@ package disposiciones;
  * @author german
  */
 public class DisposicionLibre {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
