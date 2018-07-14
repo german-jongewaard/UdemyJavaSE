67ddbb65650a736fc0c98b021f1d2f334496dc77
german <germanjongewaard@gmail.com>
2018-07-14 02:51:52 +0200

    Implementando disposiciones Spring

---
+++ src/disposiciones/PracticaSpring.java
@@ -0,0 +1,61 @@
+package disposiciones;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Spring;
+import javax.swing.SpringLayout;
+ 
+
+/**
+ *
+ * @author german
+ * 
+ */
+public class PracticaSpring {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        MarcoMuelle miMarco = new MarcoMuelle();
+        
+        miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
+
+class MarcoMuelle extends JFrame{
+    
+    public MarcoMuelle(){
+        
+        setBounds(300, 400, 1000, 350);
+        
+        LaminaMuelle miLamina = new LaminaMuelle();
+        
+        add(miLamina);
+        
+        setVisible(true);
+        
+    } 
+}
+
+class LaminaMuelle extends JPanel{
+    
+    public LaminaMuelle(){
+        
+        JButton boton1 = new JButton("Boton 1");
+        JButton boton2 = new JButton("Boton 2");
+        JButton boton3 = new JButton("Boton 3");
+        
+        SpringLayout miLayout = new SpringLayout();
+        
+        setLayout(miLayout);
+        
+        add(boton1);
+        add(boton2);
+        add(boton3);
+        
+         Spring miMuelle = Spring.constant(WIDTH, ERROR, WIDTH);
+        
+    }
+}
