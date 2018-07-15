f09b36d00c46f41ed80f0c6ff043137c70fc4f5a
german <germanjongewaard@gmail.com>
2018-07-15 03:59:52 +0200

    Implementando disposiciones Spring

--- src/disposiciones/DisposicionLibre.java
+++ src/disposiciones/DisposicionLibre.java
@@ -41,7 +41,7 @@ class LaminaDispLibre extends JPanel{
         
         setLayout(null); //Para poder trabajar con Disposiciones Libres
         
-        JButton boton1
+        JButton boton1 = new JButton()
         
         
         
