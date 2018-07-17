f67ee22653c988bd0878749eefd443288b4c681a
german <germanjongewaard@gmail.com>
2018-07-17 03:35:13 +0200

    Implementando disposiciones Libres

--- src/disposiciones/DisposicionLibre.java
+++ src/disposiciones/DisposicionLibre.java
@@ -78,27 +78,27 @@ class EnColumnas implements LayoutManager{
 
     @Override
     public void addLayoutComponent(String name, Component comp) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       
     }
 
     @Override
     public void removeLayoutComponent(Component comp) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       
     }
 
     @Override
     public Dimension preferredLayoutSize(Container parent) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       
     }
 
     @Override
     public Dimension minimumLayoutSize(Container parent) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       
     }
 
     @Override
     public void layoutContainer(Container parent) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       
     }
     
 }
