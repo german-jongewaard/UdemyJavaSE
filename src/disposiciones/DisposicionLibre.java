d18303e8fb1384ddf017b92c3c14d572671e674f
german <germanjongewaard@gmail.com>
2018-07-17 03:34:48 +0200

    Implementando disposiciones Libres

--- src/disposiciones/DisposicionLibre.java
+++ src/disposiciones/DisposicionLibre.java
@@ -1,5 +1,9 @@
 package disposiciones;
 
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.LayoutManager;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -70,4 +74,31 @@ class LaminaDispLibre extends JPanel{
     }
 }
 
-class EnColumnas 
+class EnColumnas implements LayoutManager{
+
+    @Override
+    public void addLayoutComponent(String name, Component comp) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeLayoutComponent(Component comp) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Dimension preferredLayoutSize(Container parent) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Dimension minimumLayoutSize(Container parent) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void layoutContainer(Container parent) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
