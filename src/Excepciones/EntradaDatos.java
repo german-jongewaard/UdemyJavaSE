b50a317e79d6030bca29ea15734aaea4b75c94c7
german <germanjongewaard@gmail.com>
2018-10-03 09:56:31 +0200

    Excepciones - Clausula throws - Excepciones no comprobadas.

--- src/Excepciones/EntradaDatos.java
+++ src/Excepciones/EntradaDatos.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Excepciones;
 
 /**
  *
  * @author german
+ * 
  */
 public class EntradaDatos {
 
@@ -16,6 +12,7 @@ public class EntradaDatos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("");
     }
     
 }
