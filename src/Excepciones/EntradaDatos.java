fb4f251d760ed49a17d17efc1a2ba90fc31e6b3b
german <germanjongewaard@gmail.com>
2018-10-23 10:08:34 +0200

    Excepciones - Clausula throws - Excepciones no comprobadas.

--- src/Excepciones/EntradaDatos.java
+++ src/Excepciones/EntradaDatos.java
@@ -51,7 +51,7 @@ public class EntradaDatos {
         
         int edad = entrada.nextInt();
         
-        System.out.println("Hola " + nombre_usuario + ". El año que viene tendrás " + (edad+1));
+        System.out.println("Hola " + nombre_usuario + ". El año que viene tendrás " + (edad+1) + " años");
         
     }
     
