18e7a8b9fe6f06fc3b9d64368e4b29ec02ef96c4
german <germanjongewaard@gmail.com>
2018-11-16 10:44:27 +0100

    Excepciones - Clausula throws - Excepciones no comprobadas.

---
+++ src/Debugger/Aleatorios.java
@@ -0,0 +1,33 @@
+package Debugger;
+
+import javax.swing.JOptionPane;
+import javax.swing.JRootPane;
+
+/**
+ *
+ * @author german
+ * 
+ */
+public class Aleatorios {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int elementos = Integer.parseInt(JOptionPane.showInputDialog("Introduce elementos de la matriz"));
+        
+        int num_aleat[] = new int[elementos];
+        
+        for(int i=0; i<num_aleat.length; i++){
+            
+            num_aleat[i] = (int)Math.random()*100; 
+            
+        }
+        
+        for(int elem:num_aleat){
+            
+            System.out.println(elem);
+            
+        }
+    }
+    
+}
