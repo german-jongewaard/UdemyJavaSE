a6e6fb869684564469c659a4e3d3a4b549954f19
german <germanjongewaard@gmail.com>
2018-11-22 15:15:16 +0100

    Leyendo_Escribiendo_Archivo

---
+++ src/Acceso_Fichero/Leyendo_Escribiendo_Archivo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Acceso_Fichero;
+
+/**
+ *
+ * @author german
+ */
+public class Leyendo_Escribiendo_Archivo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
