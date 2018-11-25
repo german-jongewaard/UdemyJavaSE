50ece91aca4fcf490e20d569f7c157c3a811d07f
german <germanjongewaard@gmail.com>
2018-11-25 15:17:03 +0100

    Leyendo_Escribiendo_Archivo

--- src/Acceso_Fichero/Leyendo_Escribiendo_Archivo.java
+++ src/Acceso_Fichero/Leyendo_Escribiendo_Archivo.java
@@ -4,10 +4,16 @@ package Acceso_Fichero;
  *
  * @author german
  */
+import java.io.*;
+
 public class Leyendo_Escribiendo_Archivo {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        FileInputStream lectura_archivo = new FileInputStream(file);
+        
+        
     }
     
 }
