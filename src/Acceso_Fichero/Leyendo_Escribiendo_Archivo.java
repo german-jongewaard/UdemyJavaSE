d005a8dbd3f6d37b83c2a6e319409babca06b63d
german <germanjongewaard@gmail.com>
2018-11-26 15:19:28 +0100

    Leyendo_Escribiendo_Archivo

--- src/Acceso_Fichero/Leyendo_Escribiendo_Archivo.java
+++ src/Acceso_Fichero/Leyendo_Escribiendo_Archivo.java
@@ -5,13 +5,21 @@ package Acceso_Fichero;
  * @author german
  */
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Leyendo_Escribiendo_Archivo {
 
     public static void main(String[] args) {
-        // TODO code application logic here
         
-        FileInputStream lectura_archivo = new FileInputStream(file);
+        
+        try {
+            // TODO code application logic here
+
+            FileInputStream lectura_archivo = new FileInputStream("/home/german/Escritorio/logoGerman.jpg");
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Leyendo_Escribiendo_Archivo.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
         
     }
