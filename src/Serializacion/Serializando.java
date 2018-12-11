1489e0ac11957c0e352089ad2d9ff4947daa921f
german <germanjongewaard@gmail.com>
2018-12-11 16:15:41 +0100

    Serializando

--- src/Serializacion/Serializando.java
+++ src/Serializacion/Serializando.java
@@ -1,5 +1,8 @@
 package Serializacion;
 
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 /**
  *
  * @author german
@@ -29,5 +32,75 @@ class Empleado{
     
     public Empleado(String n, double s, int agno, int mes, int dia){
         
+        nombre = n;
+        
+        sueldo = s;
+        
+        GregorianCalendar calendario = new GregorianCalendar(agno, mes-1, dia);
+        
+        fechaContrato = calendario.getTime();
     }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public double getSueldo(){
+        return sueldo;
+    }
+    
+    public Date getFechaContrato(){
+        return fechaContrato;
+    }
+    
+    public void subirSueldo(double porcentaje){
+        
+        double aumento = sueldo*porcentaje/100;
+        
+        sueldo+=aumento;
+        
+    }
+    
+    public String toString(){
+        return "El Nombre es: " + nombre + ", y su sueldo es = " + sueldo + ", fecha de contrato = " + fechaContrato;
+    }
+    
+    
+    private String nombre;
+    private double sueldo;
+    private Date fechaContrato;  
+    
+}
+
+//-----------------------------------------------------------------
+
+class Administrador extends Empleado{
+    
+    public Administrador(String n, double s, int agno, int mes, int dia){
+        
+        super(n, s, agno, mes, dia);
+        
+        incentivo = 0;
+    }
+    
+    public double getSueldo(){
+        double sueldoBase = super.getSueldo();
+        
+        return sueldoBase + incentivo;
+    }
+    
+    public void setIncentivo(double b){
+        
+        incentivo  = b;
+    }
+    
+    public String toString(){
+        
+        return super.toString() + "Incentivo = " + incentivo;
+        
+    }
+    
+    private double incentivo;
+    
+    
 }
