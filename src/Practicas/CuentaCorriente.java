a96c33a8ae0b76fd274d9b47e53702ff7f0e62aa
german <germanjongewaard@gmail.com>
2018-12-12 10:29:30 +0100

    Serializando

---
+++ src/Practicas/CuentaCorriente.java
@@ -0,0 +1,17 @@
+package Practicas;
+
+/**
+ *
+ * @author german
+ * 
+ */
+public class CuentaCorriente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
