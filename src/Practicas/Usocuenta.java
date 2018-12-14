2e462c1359bfde21c605939335c977ccbf955221
german <germanjongewaard@gmail.com>
2018-12-14 10:40:16 +0100

    Serializando

---
+++ src/Practicas/Usocuenta.java
@@ -0,0 +1,36 @@
+package Practicas;
+
+import java.util.Random;
+
+/**
+ *
+ * @author german
+ * 
+ */
+public class Usocuenta {
+    
+        double saldo;
+        String nombreTitular;
+        long numeroCuenta;
+        
+        
+        public Usocuenta(double saldo, String nombreTitular, long numeroCuenta){
+        
+            this.saldo = saldo;
+            this.nombreTitular = nombreTitular;
+            
+            Random rnd = new Random();
+            
+            
+        }
+        
+        
+    public static void main(String[] args) {
+        // TODO code application logic here
+         
+        
+         
+        
+    }
+    
+}
