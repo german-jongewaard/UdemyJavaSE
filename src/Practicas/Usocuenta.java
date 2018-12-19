6a2007fada66b321819742b5c99bfdd5f6b7cea9
german <germanjongewaard@gmail.com>
2018-12-19 10:49:14 +0100

    UsoCuenta práctica pasada.

--- src/Practicas/Usocuenta.java
+++ src/Practicas/Usocuenta.java
@@ -32,8 +32,26 @@ class CuentaCorriente {
             this.nombreTitular = nombreTitular;
             
             Random rnd = new Random();
-            numeroCuenta = Math.abs(rnd.nextLong());
-            
-            
+            numeroCuenta = Math.abs(rnd.nextLong()); 
         }
+        
+        public void setIngreso(double ingreso){
+            
+            if(ingreso < 0) System.out.println("No se permiten ingresos negativos");
+            
+            else saldo += ingreso;
+        }
+        
+        public void setReingreso(double reingreso){
+            
+            saldo += reingreso;
+        }
+        
+        public String getSaldo(){
+            return "El saldo de la cuenta es: " + saldo;
+        }
+        
+        
+        
+        
 }
