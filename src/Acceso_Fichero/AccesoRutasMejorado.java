b5d8cd526196f64bc8d262612812fbcb2f4b66f2
german <germanjongewaard@gmail.com>
2019-01-12 09:51:01 +0100

    Acceso a diferentes rutas. java.io

---
+++ src/Acceso_Fichero/AccesoRutasMejorado.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Acceso_Fichero;
+
+import java.io.File;
+
+/**
+ *
+ * @author german
+ */
+public class AccesoRutasMejorado {
+
+   public static void main(String[] args) {
+        // TODO code application logic here
+        
+        File directorio = new File("/home/german/Escritorio/externo");
+        
+        System.out.println(directorio.getAbsolutePath());
+        
+        //System.out.println(archivo.exists());
+        
+        String [] nombres = directorio.list();
+        
+        for (int i = 0; i < nombres.length; i++) {
+            
+            System.out.println(nombres[i]);   
+            
+            File nuevoDirectorio = new File(directorio.getAbsolutePath(), nombres[i]);
+            
+            
+            
+            if(nuevoDirectorio.isDirectory()){                
+                
+                String [] archivos_subcarpeta = nuevoDirectorio.list();
+                
+                for (int j = 0; j < archivos_subcarpeta.length; j++) {
+                    
+                 System.out.println(archivos_subcarpeta[j] + "--> proviene del directorio " + nuevoDirectorio.getAbsolutePath());   
+                 
+                }
+                 
+            }
+         
+             
+
+        }
+         
+        
+        
+    }
+    
+}
