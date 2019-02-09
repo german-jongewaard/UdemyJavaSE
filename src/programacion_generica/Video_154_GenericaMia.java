ab2d824b8282766a3ad451915f7fd535e3a4f90b
german <germanjongewaard@gmail.com>
2019-02-09 11:41:42 +0100

    Video 154 PROGRAMACIÓN GENÉRICA

--- src/programacion_generica/Video_154_GenericaMia.java
+++ src/programacion_generica/Video_154_GenericaMia.java
@@ -6,6 +6,24 @@ package programacion_generica;
  */
 public class Video_154_GenericaMia<T> {
 
+    
+    public Video_154_GenericaMia(){
+        
+        objecto = null;
+        
+    }
+    
+    
+    
+    public void setObjeto(T nuevoValor) {
+        
+        objeto
+    }
+    
+    
+    
+    
+    private T objecto;
      
     
 }
