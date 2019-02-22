f6f32890f101e73e340a321a989ad1d4663c543e
german <germanjongewaard@gmail.com>
2019-02-22 16:28:34 +0100

    Video 157 Herencia Genéricos Parámetros comodín

---
+++ src/programacion_generica/Video_157_Jefe.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacion_generica;
+
+/**
+ *
+ * @author german
+ */
+public class Video_157_Jefe extends Video_157_Empleado{
+    
+    public Video_157_Jefe(String nombre, int edad, double salario) {
+        super(nombre, edad, salario);
+    }
+    
+    double incentivo(double inc){
+        
+        return inc;
+    }
+    
+    
+}
