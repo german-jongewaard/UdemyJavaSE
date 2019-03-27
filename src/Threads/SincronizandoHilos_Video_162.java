c28c3937b4e8b9296ed97f104de6df50e9e27a55
german-jongewaard <germanjongewaard@gmail.com>
2019-03-27 10:29:29 +0100

    Threads - SincronizandoHilos III

--- src/Threads/SincronizandoHilos_Video_162.java
+++ src/Threads/SincronizandoHilos_Video_162.java
@@ -13,27 +13,10 @@ public class SincronizandoHilos_Video_162 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        SincronizaThreads hilo1 = new SincronizaThreads();
-        SincronizaThreads hilo2 = new SincronizaThreads();
+        SincronizaThreadsIII hilo1 = new SincronizaThreadsIII();
+        SincronizaThreadsIV hilo2 = new SincronizaThreadsIV(hilo1);
         hilo1.start();
-        
-        try {
-            //ejecuta primero el hilo1, hasta que no muera el hilo1 no comienza el hilo2
-            hilo1.join(); //join() espera hasta que el hilo este muerto
-            
-        } catch (InterruptedException ex) {
-           ex.printStackTrace();
-        }
          
-         hilo2.start();
-        
-        try {
-            hilo2.join();
-        } catch (InterruptedException ex) {
-            ex.printStackTrace();
-        }
-        
-        System.out.println("Hemos terminado las tareas!!");
     }
     
 }
