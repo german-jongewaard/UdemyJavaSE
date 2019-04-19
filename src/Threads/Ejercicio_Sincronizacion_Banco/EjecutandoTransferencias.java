4fcce86c2837414b4e92f5cf2ef0f66bb21b6c3b
german-jongewaard <germanjongewaard@gmail.com>
2019-04-19 09:53:20 +0200

    Threads - Hilos (Ejemplo Banco)

---
+++ src/Threads/Ejercicio_Sincronizacion_Banco/EjecutandoTransferencias.java
@@ -0,0 +1,20 @@
+package Threads.Ejercicio_Sincronizacion_Banco;
+
+/**
+ *
+ * @author german
+ */
+public class EjecutandoTransferencias implements Runnable{
+
+    @Override
+    public void run() {
+        
+    }
+    
+    private Banco miBanco;
+    
+    private int cuentaDeOrigen;
+    
+    private double cantidadMaxima;
+    
+}
