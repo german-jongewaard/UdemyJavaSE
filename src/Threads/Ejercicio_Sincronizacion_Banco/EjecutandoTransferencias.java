04a7b3c0d278448f281412dbc25570243739218a
german-jongewaard <germanjongewaard@gmail.com>
2019-04-19 10:01:20 +0200

    Threads - Hilos (Ejemplo Banco)

--- src/Threads/Ejercicio_Sincronizacion_Banco/EjecutandoTransferencias.java
+++ src/Threads/Ejercicio_Sincronizacion_Banco/EjecutandoTransferencias.java
@@ -6,9 +6,23 @@ package Threads.Ejercicio_Sincronizacion_Banco;
  */
 public class EjecutandoTransferencias implements Runnable{
 
+    public EjecutandoTransferencias(Banco miBanco, int cuentaDeOrigen, double cantidadMaxima) {
+        this.miBanco = miBanco;
+        this.cuentaDeOrigen = cuentaDeOrigen;
+        this.cantidadMaxima = cantidadMaxima;
+    }
+    
     @Override
     public void run() {
         
+        while(true){ //bucle infinito
+            
+            int cuentaDeDestino = (int)(Math.random()*100); //genera un numero aleatorio entre 0 y 100
+            
+            double cantidadATransferir = cantidadMaxima*Math.random();
+         
+            miBanco.transferencias(cuentaDeOrigen, cuentaDeDestino, cantidadATransferir);
+        }        
     }
     
     private Banco miBanco;
