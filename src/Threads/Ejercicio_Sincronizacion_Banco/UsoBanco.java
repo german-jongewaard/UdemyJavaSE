0df019acdf2cc8a592f57aea7ffb2bc8eec37e7b
german-jongewaard <germanjongewaard@gmail.com>
2019-04-20 10:06:04 +0200

    Threads - Hilos (Ejemplo Banco)

--- src/Threads/Ejercicio_Sincronizacion_Banco/UsoBanco.java
+++ src/Threads/Ejercicio_Sincronizacion_Banco/UsoBanco.java
@@ -11,6 +11,15 @@ public class UsoBanco {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Banco b = new Banco();
+        
+        for(int i=0; i<100;i++){
+            /*Aquí creo una instancia r, donde ejecuta trnsferencias, 
+            donde: b es el banco, i es la cntidad de transferencias
+            y 2000 el importe a transferir */
+            EjecutandoTransferencias r =new EjecutandoTransferencias(b, i, 2000);
+        }
     }
     
 }
