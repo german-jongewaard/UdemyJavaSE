88d3f5c43d55130f91b9cbeba901471e01dffc24
german-jongewaard <germanjongewaard@gmail.com>
2019-06-24 12:54:05 +0200

    Colecciones VII. LinkedList o Listas Enlazadas.

---
+++ src/Colecciones/LinkedListTest.java
@@ -0,0 +1,23 @@
+package Colecciones;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author german
+ */
+public class LinkedListTest {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        LinkedList<String> personas = new LinkedList<String>();
+        
+        personas.add("Ana");
+        personas.add("Antonio");
+        personas.add("Sara");
+        personas.add("Pedro");
+        
+    }
+    
+}
