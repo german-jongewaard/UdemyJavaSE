58c399e4b9b2ae007cff37b84156e6704f024541
german-jongewaard <germanjongewaard@gmail.com>
2019-06-27 12:29:21 +0200

    Colecciones II. Sintaxis y creación de colecciones

---
+++ src/Colecciones/ClientesBancarios.java
@@ -0,0 +1,34 @@
+package Colecciones;
+
+import java.util.*;
+
+/**
+ *
+ * @author german
+ */
+public class ClientesBancarios {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Cliente cliente1 = new Cliente("Antonio Lopez", "0001", 20000);
+        Cliente cliente2 = new Cliente("Rafael Gómez", "0002", 15000);
+        Cliente cliente3 = new Cliente("Ana Martín", "0003", 200000);
+        Cliente cliente4 = new Cliente("Juan Díaz", "0004", 20);
+        
+
+        //Coleccion
+        Set<Cliente> clientesDelBanco = new HashSet<Cliente>();
+        
+        clientesDelBanco.add(cliente1);
+        clientesDelBanco.add(cliente2);
+        clientesDelBanco.add(cliente3);
+        clientesDelBanco.add(cliente4);
+        
+        for (Cliente cliente : clientesDelBanco) {
+            
+            System.out.println(cliente.getNombre() + " " + cliente.getnCuenta() 
+                    + " " + cliente.getSaldo());            
+        }        
+    }    
+}
