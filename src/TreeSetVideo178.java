65bc102b778b1f398a24d00a4e98a4b84b3e117b
german-jongewaard <germanjongewaard@gmail.com>
2019-09-09 12:46:04 +0200

    Colecciones VIII. TreeSet V-178

--- src/TreeSetVideo178.java
+++ src/TreeSetVideo178.java
@@ -1,14 +1,45 @@
+
+import java.util.TreeSet;
+
 /**
  *
  * @author german
  */
+
 public class TreeSetVideo178 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        TreeSet<String> ordenaGente = new TreeSet<String>();
+        
+        ordenaGente.add("Oscar");
+        ordenaGente.add("María");
+        ordenaGente.add("Ana");
+        
+        for(String g:ordenaGente){
+            System.err.println(g);
+        }
     }
+    
+}
+
+class Productos implements Comparable<Productos>{
+    
+    //constructor
+    public Productos(int numeroProducto, String descripcion){
+        
+        this.numeroProducto = numeroProducto;
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public int compareTo(Productos o) {
+        
+        return 0;
+    }
+    
+    private int numeroProducto;
+    private String descripcion;
     
 }
