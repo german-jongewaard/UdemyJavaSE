919623e12ea8ca9429a3b12674d21abfc87a753b
german-jongewaard <germanjongewaard@gmail.com>
2019-09-09 16:48:08 +0200

    Colecciones VIII. TreeSet V-178

---
+++ src/Colecciones/UsoMapasV180.java
@@ -0,0 +1,56 @@
+package Colecciones;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author german
+ */
+
+public class UsoMapasV180 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        HashMap<String, Empleado> listaEmpleados = new HashMap<String, Empleado>();
+        
+        
+        listaEmpleados.put("100", new Empleado("Juan"));
+        
+        listaEmpleados.put("101", new Empleado("Antonio"));
+        
+        listaEmpleados.put("102", new Empleado("María"));
+        
+        listaEmpleados.put("103", new Empleado("Lucía"));
+        
+        System.err.println(listaEmpleados);
+        
+        listaEmpleados.remove("101");
+        
+        System.err.println(listaEmpleados);
+        
+        listaEmpleados.put("103", new Empleado("Ana"));
+        
+        System.err.println(listaEmpleados);
+
+        
+    }    
+}
+
+class Empleado{
+
+    public Empleado(String nombre) {
+        this.nombre = nombre;
+        salario = 2000;
+    }
+
+    @Override
+    public String toString() {
+        return "Epmpleado{" + "nombre=" + nombre + ", salario=" + salario + '}';
+    } 
+      
+    
+    
+    private String nombre;
+    private double salario;
+}
