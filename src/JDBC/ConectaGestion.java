b5f3ad25c86e9d02d8d4ea751cd4fb09b8f06e40
german-jongewaard <germanjongewaard@gmail.com>
2020-02-26 15:23:03 +0100

    Connection to JDBC

---
+++ src/JDBC/ConectaGestion.java
@@ -0,0 +1,13 @@
+package JDBC;
+
+/**
+ *
+ * @author german
+ */
+public class ConectaGestion {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+            
+    }    
+}
